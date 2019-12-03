3ea529491fa6c52a454bf193331432a795823641
Gian <gianvs@live.com>
2019-12-03 14:38:14 -0300

    - Switched from a local postgres SQL database to an online MySQL database; - Switched package used to connect to database and to make queries (psycopg2 to SQLAlchemy) - Removed 'amazon_table.py' which contained the data acquiring queries. This file was used mainly to improve the main code readability

--- copy_csv_to_table.py
+++ copy_csv_to_table.py
@@ -1,10 +1,10 @@
 import pandas as pd
 ###This file was used to copy the data from "amazon.csv" to a table in a local database
 
-data = pd.read_csv('/home/gian/PycharmProjects/forest_fires/amazon.csv')
+data = pd.read_csv('amazon.csv')
 data.columns = [c.lower() for c in data.columns] #postgres doesn't like capitals or spaces
 
 from sqlalchemy import create_engine
-engine = create_engine('postgresql://gian:teste@localhost:5432/gian')
-
+# engine = create_engine('postgresql://gian:teste@localhost:5432/gian')
+engine = create_engine('mysql+pymysql://WxVyhiGBXQ:RKAsHKo3zg@remotemysql.com:3306/WxVyhiGBXQ')
 data.to_sql('amazon', con=engine)
